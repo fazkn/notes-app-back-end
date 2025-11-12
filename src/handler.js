93b8637007c08672ffe6536d699f0c50382f906f
fazkn <fazakamila5@gmail.com>
2025-11-12 10:58:22 +0700

    Feat(notes): implement GET /notes and GET /notes/{id} endpoints
    Added functionality to retrieve all notes and specific notes by ID.
    This completes the display feature for stored notes and enables the client app to fetch data from the server.

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,5 @@
 const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
@@ -34,4 +35,33 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
